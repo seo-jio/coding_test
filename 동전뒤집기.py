d4df6281e2264184227962f6f3703878861ad486
seo-jio <lio8625@naver.com>
2022-10-04 00:13:23 +0900

    Fix : 동전 뒤집기

--- 동전뒤집기.py
+++ 동전뒤집기.py
@@ -5,23 +5,19 @@ for i in range(t):
 
 #1 앞면, 0 뒷면
 for i in range(t):
-
-
-
-# for i in range(t):
-#     temp = ""
-#     for _ in range(len(arr[i])):
-#         temp += "0"
-#     count = 0
-#     while True:
-#         # print(f"arr[i] = {arr[i]}, temp = {temp}")
-#         if arr[i] == temp:
-#             break
-#         for a in range(len(arr[i])):
-#             if arr[i][a] != temp[a]:
-#                 t = temp[:a] + arr[i][a]
-#                 for _ in range(len(temp[a+1:])):
-#                     t += arr[i][a]
-#                 temp = t
-#                 count += 1
-#     print(f"#{i+1} {count}")
+    temp = ""
+    for _ in range(len(arr[i])):
+        temp += "0"
+    count = 0
+    while True:
+        # print(f"arr[i] = {arr[i]}, temp = {temp}")
+        if arr[i] == temp:
+            break
+        for a in range(len(arr[i])):
+            if arr[i][a] != temp[a]:
+                t = temp[:a] + arr[i][a]
+                for _ in range(len(temp[a+1:])):
+                    t += arr[i][a]
+                temp = t
+                count += 1
+    print(f"#{i+1} {count}")
