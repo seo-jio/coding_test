97130ed575cffece25383689cdeecbeabf21d9af
seo-jio <lio8625@naver.com>
2022-10-26 17:24:47 +0900

    Fix : 최대 최소간의 차

--- 코드트리/BruteForce/BruteForce3/기준을_새로_설정하여_완전탐색/최대_최소간의_차.py
+++ 코드트리/BruteForce/BruteForce3/기준을_새로_설정하여_완전탐색/최대_최소간의_차.py
@@ -2,26 +2,20 @@ import sys
 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-print(arr)
 
 def cal_cost(i):
     cost = 0
     for a in arr:
-        if i <= a <= i + k:
+        if i <= a <= i + k: #범위 안에 있을 경우
             continue
         elif a < i:
-            print("first")
             cost += i - a
         else:
-            print("third")
             cost += a - (i+k)
-    print(f"cost : {cost}")
     return cost
 
 min_cost = sys.maxsize
-for i in range(1, 1000 + 1):
-    print(f"i : {i}")
+for i in range(1, 10000 + 1): #문제에서 주어진 범위를 제발 잘 좀 보자....
     cost = cal_cost(i)
-    print("======================")
     min_cost = min(min_cost, cost)
 print(min_cost)
