8a2fc1111bf18ee7356774ea4af95826687e00fe
seojio <lio8625@naver.com>
2023-02-08 15:02:15 +0900

    BJ_1789

---
+++ 백준/BJ_1789.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class P_1789 {
+    static int S;
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        long S = sc.nextLong();
+        Long cnt = 1L;
+        Long sum = 0L;
+        while(true) {
+            sum += cnt;
+            if (sum > S) {
+                break;
+            }
+            cnt++;
+        }
+        System.out.println(cnt-1);
+    }
+}
