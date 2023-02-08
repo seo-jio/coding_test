c04ab6ad583037018adc0041f786dfc3144a5f64
seojio <lio8625@naver.com>
2023-02-08 15:02:57 +0900

    BJ_178 수정

--- 백준/BJ_1789.java
+++ 백준/BJ_1789.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class P_1789 {
+public class BJ_1789 {
     static int S;
     public static void main(String[] args) throws IOException {
 
